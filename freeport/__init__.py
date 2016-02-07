e4b772c40f5efd153b908167aa95578e5fc0315f
Yash Bathia <yash.bathia@yahoo.com>
2016-02-06 16:30:03 -0800

    Adding freeport script

--- freeport/__init__.py
+++ freeport/__init__.py
@@ -3,3 +3,20 @@
 __author__ = 'Yash Bathia'
 __email__ = 'yash.bathia@yahoo.com'
 __version__ = '0.1.0'
+import json
+import os
+
+from freeport import *
+
+
+_metadata_file = os.path.join(
+    os.path.dirname(__file__),
+    'package_metadata.json'
+)
+
+if os.path.exists(_metadata_file):  # pragma: no cover
+    with open(_metadata_file) as fh:
+        _package_metadata = json.load(fh)
+        __version__ = _package_metadata['version']
+else:
+    __version__ = '0.0.0'  # pragma: no cover
--- setup.py
+++ setup.py
@@ -1,58 +1,137 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import json
+from setuptools import setup
 
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# Package Metadata filename
+METADATA_FILENAME = 'freeport/package_metadata.json'
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    # TODO: put package requirements here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-]
-
-setup(
-    name='freeport',
-    version='0.1.0',
-    description="Simple Python library to free a port",
-    long_description=readme + '\n\n' + history,
-    author="Yash Bathia",
-    author_email='yash.bathia@yahoo.com',
-    url='https://github.com/yashbathia/freeport',
-    packages=[
-        'freeport',
+# Create a dictionary of our arguments, this way this script can be imported
+# without running setup() to allow external scripts to see the setup settings.
+setup_arguments = {
+    'name': 'freeport',
+    'version': '0.1.2',
+    'author': 'Yash Bathia',
+    'author_email': 'yash.bathia@yahoo.com',
+    'url': 'http://github.com/yashbathia/freeport',
+    'license': 'ISC',
+    'packages': ['freeport'],
+    'long_description': 'Simple Python library to free a port',
+    'description': 'Simple Python library to free a port',
+    'classifiers': [
+            'License :: OSI Approved :: ISC Licese',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
     ],
-    package_dir={'freeport':
-                 'freeport'},
-    include_package_data=True,
-    install_requires=requirements,
-    license="ISCL",
-    zip_safe=False,
-    keywords='freeport',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+    'package_data': {
+        'freeport': ['package_metadata.json']
+    },
+    'include_package_data': True,
+}
+
+
+class Git(object):
+    """
+    Simple wrapper class to the git command line tools
+    """
+    version_list = ['0', '7', '0']
+
+    def __init__(self, version=None):
+        if version:
+            self.version_list = version.split('.')
+
+    @property
+    def version(self):
+        """
+        Generate a Unique version value from the git information
+        :return:
+        """
+        git_rev = len(os.popen('git rev-list HEAD').readlines())
+        if git_rev != 0:
+            self.version_list[-1] = '%d' % git_rev
+        version = '.'.join(self.version_list)
+        return version
+
+    @property
+    def branch(self):
+        """
+        Get the current git branch
+        :return:
+        """
+        return os.popen('git rev-parse --abbrev-ref HEAD').read().strip()
+
+    @property
+    def hash(self):
+        """
+        Return the git hash for the current build
+        :return:
+        """
+        return os.popen('git rev-parse HEAD').read().strip()
+
+    @property
+    def origin(self):
+        """
+        Return the fetch url for the git origin
+        :return:
+        """
+        for item in os.popen('git remote -v'):
+            split_item = item.strip().split()
+            if split_item[0] == 'origin' and split_item[-1] == '(push)':
+                return split_item[1]
+
+
+def add_scripts_to_package():
+    """
+    Update the "scripts" parameter of the setup_arguments with any scripts
+    found in the "scripts" directory.
+    :return:
+    """
+    global setup_arguments
+
+    if os.path.isdir('scripts'):
+        setup_arguments['scripts'] = [
+            os.path.join('scripts', f) for f in os.listdir('scripts')
+        ]
+
+
+def get_and_update_package_metadata():
+    """
+    Update the package metadata for this package if we are building the package.
+    :return:metadata - Dictionary of metadata information
+    """
+    global setup_arguments
+    global METADATA_FILENAME
+
+    if not os.path.exists('.git') and os.path.exists(METADATA_FILENAME):
+        with open(METADATA_FILENAME) as fh:
+            metadata = json.load(fh)
+    else:
+        git = Git(version=setup_arguments['version'])
+        metadata = {
+            'version': git.version,
+            'long_description': 'Tool to pip install missing imports and more',
+            'git_hash': git.hash,
+            'git_origin': git.origin,
+            'git_branch': git.branch
+        }
+        for readme_file in ['README.rst', 'README.md', 'README.txt']:
+            if os.path.exists(readme_file):
+                with open(readme_file) as file_handle:
+                    metadata['long_description'] = file_handle.read()
+                break
+        with open(METADATA_FILENAME, 'w') as fh:
+            json.dump(metadata, fh)
+    return metadata
+
+
+if __name__ == '__main__':
+    # We're being run from the command line so call setup with our arguments
+    metadata = get_and_update_package_metadata()
+    setup_arguments['version'] = metadata['version']
+    setup_arguments['long_description'] = metadata['long_description']
+    add_scripts_to_package()
+    setup(**setup_arguments)
